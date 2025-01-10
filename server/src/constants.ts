79bd50a751631001fb12c2681af3ff0df5c01754
Mohammed Farshad <imohammedfarshad@gmail.com>
2025-01-10 19:56:59 +0530

    add cloudinary connection

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -3,5 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT;
+
 export const MONGO_DB_URI = process.env.MONGO_DB_URI;
 export const DB_NAME = "document-status-tracker";
+
+export const CLOUDINARY_CLOUD_NAME = process.env.ClOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.ClOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.ClOUDINARY_API_SECRET;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,19 @@
 import { app } from "./app";
-import { PORT } from "./constants";
+import {
+  PORT,
+  CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+} from "./constants";
 import { connectDB } from "./db";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 connectDB()
   .then(() => {
